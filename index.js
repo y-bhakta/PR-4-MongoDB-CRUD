9a04b575a03e3fe574b1a9cb3610a1e9adc6a276
y-bhakta <ybhaktaace11@gmail.com>
2025-10-14 10:49:01 +0530

    update

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 import express from "express"
 import dotenv from "dotenv"
 import { db } from "./configs/db.js"
+import { User } from "./models/usermodle.js"
+import bodyParser from "body-parser"
 dotenv.config();
-const app=express();
-const port=process.env.PORT||8081;
-app.listen(port,(err)=>{
-    db
-    if(err){
-        console.log(err);        
+const port = process.env.PORT || 8081;
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post("/user/create", (req, res) => {
+    User.create(req.body)
+        .then((data) => {
+            return res.json(data);
+        })
+        .catch((err) => {
+            return res.json({ message: err.message });
+        });
+});
+app.get("/user/getallusers", (req, res) => {
+    User.find({})
+        .then((data) => {
+            return res.json(data);
+        })
+        .catch((err) => {
+            return res.json({ message: err.message });
+        });
+});
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err);
     }
-    console.log("Server Stared At port http://127.0.0.1:"+port);
+    console.log("Server Stared At port http://127.0.0.1:" + port);
 });
---
+++ models/usermodle.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+const UserSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true
+    },
+    password:{
+        type:String,
+        required:true,
+    }
+},{
+    timestamps:true
+});
+
+export const User=mongoose.model("User",UserSchema);
