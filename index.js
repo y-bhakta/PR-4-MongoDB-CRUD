56e20d14d0125916371ed3809faec72f64dbe877
y-bhakta <ybhaktaace11@gmail.com>
2025-10-16 10:45:11 +0530

    rest api

--- index.js
+++ index.js
@@ -6,8 +6,44 @@ import bodyParser from "body-parser"
 dotenv.config();
 const port = process.env.PORT || 8081;
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.post("/user/create", (req, res) => {
+app.use(bodyParser.json());
+app.get('/',(req,res)=>{
+    res.render("./index");
+});
+app.get('/form-basic',(req,res)=>{
+    res.render("./pages/form-basic");
+});
+app.get('/tables',(req,res)=>{
+    res.render("./pages/tables");
+});
+app.get('/signup',(req,res)=>{
+    return res.render('./pages/signup');
+});
+app.get('/login',(req,res)=>{
+    return res.render('./pages/login');
+});
+app.post('/signup',(req,res)=>{
+    fetch('http://127.0.0.1:6060/user/create',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(req.body)
+    })
+    .then((rs)=>{
+        return rs.json();
+    })
+    .then((data)=>{
+        return res.redirect('/login');
+    })
+    .catch((err)=>{
+        return res.redirect('/');
+    })
+});
+app.post("/user/create", (req, res) => {    
     User.create(req.body)
         .then((data) => {
             return res.json(data);
